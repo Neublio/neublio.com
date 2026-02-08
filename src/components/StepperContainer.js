367f79e803adfc86054d29fb6f2c70f9c97df1d7
abesamma <sammadiscerning@gmail.com>
2026-02-09 00:28:26 +1100

    Add license header lines

--- src/components/StepperContainer.js
+++ src/components/StepperContainer.js
@@ -1,3 +1,8 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 /* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import { withStyles } from '@mui/styles';
